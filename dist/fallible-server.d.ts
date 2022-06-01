d85f524d6b0aa1c6191bda8ce18f4a81eadab481
Gregor Smith <gregor_smith@outlook.com>
2022-06-01 22:18:55 +0100

    export constants

--- dist/fallible-server.d.ts
+++ dist/fallible-server.d.ts
@@ -1,3 +1,4 @@
 export * from './types.js';
 export * from './server.js';
 export * from './utils.js';
+export * from './constants.js';
--- dist/fallible-server.js
+++ dist/fallible-server.js
@@ -1,4 +1,5 @@
 export * from './types.js';
 export * from './server.js';
 export * from './utils.js';
+export * from './constants.js';
 //# sourceMappingURL=fallible-server.js.map
